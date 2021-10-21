1d68c78fb0d52f499b35a2bfbb7d377d39845688
whjr2021 <91585088+whjr2021@users.noreply.github.com>
2021-10-21 12:15:09 +0530

    Add files via upload

---
+++ C4_TA1_Solution.py
@@ -0,0 +1,8 @@
+# TA 1a: Code to print the numbers 0 to 4 using a for loop
+for i in range(0, 5):
+  print(i)
+
+
+# TA 1b: Code to print x-coordinates of the bricks using a for loop
+for i in range(0,6):
+  print("For i =", i, ", bricks coordinate =", i*100+10)
